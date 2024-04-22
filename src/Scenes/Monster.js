2593fe3264c201a4f4e68fa10f0cdc4a2a96a7a9
Tyler T <151409229+tyler-torr@users.noreply.github.com>
2024-04-21 23:43:03 -0700

    Update Monster.js

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -25,6 +25,11 @@ class Monster extends Phaser.Scene {
     create() {
         let my = this.my;   // create an alias to this.my for readability
 
+        this.aKey = this.input.keyboard.addKey('A');
+        this.dKey = this.input.keyboard.addKey('D');
+        this.sKey = this.input.keyboard.addKey('S');
+        this.fKey = this.input.keyboard.addKey('F');
+
         // Create the main body sprite
         //
         // this.add.sprite(x,y, "{atlas key name}", "{name of sprite within atlas}")
@@ -32,14 +37,41 @@ class Monster extends Phaser.Scene {
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
         my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
-
-        
+        my.sprite.arm1 = this.add.sprite(this.bodyX-90, this.bodyY+50, "monsterParts", "arm_greenD.png");
+        my.sprite.arm1.flipX = true;
+        my.sprite.arm2 = this.add.sprite(this.bodyX+90, this.bodyY+50, "monsterParts", "arm_greenD.png");
+        my.sprite.eye = this.add.sprite(this.bodyX, this.bodyY-30, "monsterParts", "eye_psycho_dark.png");
+        my.sprite.leg1 = this.add.sprite(this.bodyX-40, this.bodyY+150, "monsterParts", "leg_greenA.png");
+        my.sprite.leg1.flipX = true;
+        my.sprite.leg2 = this.add.sprite(this.bodyX+40, this.bodyY+150, "monsterParts", "leg_greenA.png");
+        my.sprite.fang = this.add.sprite(this.bodyX, this.bodyY+40, "monsterParts", "mouth_closed_fangs.png");
+        my.sprite.smile = this.add.sprite(this.bodyX, this.bodyY+40, "monsterParts", "mouthA.png");
+        my.sprite.fang.visible = false;
     }
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
+        if (this.aKey.isDown) {
+            for (let bodyPart in my.sprite) {
+                my.sprite[bodyPart].x -= 1;
+            }
+        }
 
-       
+        if (this.dKey.isDown) {
+            for (let bodyPart in my.sprite) {
+                my.sprite[bodyPart].x += 1;
+            }
+       }
+
+       if (this.sKey.isDown) {
+            my.sprite.smile.visible = true;
+            my.sprite.fang.visible = false;
+       }
+
+       if (this.fKey.isDown) {
+        my.sprite.smile.visible = false;
+        my.sprite.fang.visible = true;
+   }
     }
 
 }
